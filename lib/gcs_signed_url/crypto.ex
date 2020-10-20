1cc4a4a06171e2fe1b4264645e30d756c81c5c2b
Mohamed Boudra <boudra.moha@gmail.com>
2020-10-20 10:09:33 +0200

    fix: credo warning about using get_env at compile time

--- lib/gcs_signed_url/crypto.ex
+++ lib/gcs_signed_url/crypto.ex
@@ -5,12 +5,6 @@ defmodule GcsSignedUrl.Crypto do
 
   alias GcsSignedUrl.{Client, SignBlob}
 
-  @sign_blob_http Application.get_env(
-                    :gcs_signed_url,
-                    GcsSignedUrl.SignBlob.HTTP,
-                    GcsSignedUrl.SignBlob.HTTP
-                  )
-
   @doc """
   If you pass a `%GcsSignedUrl.Client{}` as second argument, this function signs the given string with the given
   client's private key.
@@ -48,7 +42,14 @@ defmodule GcsSignedUrl.Crypto do
   defp do_post_request(string_to_sign, oauth_config) do
     payload = Base.encode64(string_to_sign)
 
-    @sign_blob_http.post(
+    sign_blob_http =
+      Application.get_env(
+        :gcs_signed_url,
+        GcsSignedUrl.SignBlob.HTTP,
+        GcsSignedUrl.SignBlob.HTTP
+      )
+
+    sign_blob_http.post(
       oauth_config.service_account,
       %{payload: payload},
       Authorization: "Bearer #{oauth_config.access_token}"
